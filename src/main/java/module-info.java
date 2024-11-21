f16ec9f68f53093dfba02a11bec34f133c163b43
dot <1310769510@qq.com>
2024-11-21 17:07:31 +0800

    Charo(*): Move project to non-maven version

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.cn.xjtlu.cpt111.cw3.cw3ljfx {
-  requires xjtlu.cpt111.assignment.quiz.lib;
-  requires java.logging;
-  requires java.desktop;
-
-  //opens edu.cn.xjtlu.cpt111.cw3.cw3ljfx to javafx.fxml;
-  exports edu.cn.xjtlu.cpt111.cw3.cw3ljfx;
-}
